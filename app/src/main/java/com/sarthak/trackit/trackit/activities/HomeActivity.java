8574db1ca1d4f7fc5cba32043bfe6135b254acf7
karan <karan26796@gmail.com>
2018-03-09 21:47:05 +0530

    "temp"

--- app/src/main/java/com/sarthak/trackit/trackit/activities/HomeActivity.java
+++ app/src/main/java/com/sarthak/trackit/trackit/activities/HomeActivity.java
@@ -1,8 +1,7 @@
-package com.sarthak.trackit.trackit;
+package com.sarthak.trackit.trackit.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -13,15 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.FirebaseAuth;
+import com.sarthak.trackit.trackit.fragments.GroupsFragment;
+import com.sarthak.trackit.trackit.fragments.MapsFragment;
+import com.sarthak.trackit.trackit.R;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 

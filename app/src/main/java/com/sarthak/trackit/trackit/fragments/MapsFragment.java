dd76b1dc574deddb19788da905719c7e152a5c4d
zaarthak <ssarthakgrover365@gmail.com>
2018-03-30 14:10:48 +0530

    Added location update on item click (local commit)

--- app/src/main/java/com/sarthak/trackit/trackit/fragments/MapsFragment.java
+++ app/src/main/java/com/sarthak/trackit/trackit/fragments/MapsFragment.java
@@ -661,27 +661,29 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, Locati
         protected void onPostExecute(List<List<HashMap<String, String>>> result) {
             ArrayList<LatLng> points = new ArrayList<>();
             PolylineOptions lineOptions = new PolylineOptions();
-            MarkerOptions markerOptions = new MarkerOptions();
+            //MarkerOptions markerOptions = new MarkerOptions();
 
-            for (int i = 0; i < result.size(); i++) {
+            if (result != null) {
 
-                List<HashMap<String, String>> path = result.get(i);
+                for (int i = 0; i < result.size(); i++) {
 
-                for (int j = 0; j < path.size(); j++) {
-                    HashMap point = path.get(j);
+                    List<HashMap<String, String>> path = result.get(i);
 
-                    double lat = Double.parseDouble((String) point.get("lat"));
-                    double lng = Double.parseDouble((String) point.get("lng"));
-                    LatLng position = new LatLng(lat, lng);
+                    for (int j = 0; j < path.size(); j++) {
+                        HashMap point = path.get(j);
 
-                    points.add(position);
+                        double lat = Double.parseDouble((String) point.get("lat"));
+                        double lng = Double.parseDouble((String) point.get("lng"));
+                        LatLng position = new LatLng(lat, lng);
+
+                        points.add(position);
+                    }
+
+                    lineOptions.addAll(points);
+                    lineOptions.width(12);
+                    lineOptions.color(Color.CYAN);
+                    lineOptions.geodesic(true);
                 }
-
-                lineOptions.addAll(points);
-                lineOptions.width(12);
-                lineOptions.color(Color.CYAN);
-                lineOptions.geodesic(true);
-
             }
 
             // Drawing polyline in the Google Map for the i-th route
